dd0a0c45a046b1e769e489e278126974165f8aca
Abhay Deep <abhay9456234282@gmail.com>
2025-08-29 22:33:16 +0530

    Game with Integers

---
+++ Codeforces - Codeforces Round 909 Div 3/A. Game with Integers/Main.java
@@ -0,0 +1,73 @@
+//  A. Game with Integers
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static FastScanner fs = new FastScanner(System.in);
+    static PrintWriter out = new PrintWriter(System.out);
+
+    public static void main(String[] args) {
+        int t = 1;
+        t = fs.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+        int n = fs.nextInt();
+
+        if((n+1)%3==0 || (n-1)%3==0 ) {
+            out.println("First");
+        }else{
+            out.println("Second");
+        }
+    }
+
+    // ---------- Fast Input ----------
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream is) {
+            br = new BufferedReader(new InputStreamReader(is));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
